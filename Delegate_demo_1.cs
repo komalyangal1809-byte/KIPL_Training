bfcbdf5a0174b680938dac86f0569e62699d4e0f
komalyangal1809-byte <komalyangal1809@gmail.com>
2025-12-15 11:52:37 +0530

    Initial commit -c# delegates Examples

---
+++ Delegate_demo_1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+delegate void myAction();
+namespace DELEGATES_CSHARP
+{
+    internal class Delegate_demo_1
+    {
+         public void SayHello()
+         {
+            Console.WriteLine("Hello");
+         }
+        public  void SayBye()
+         {
+            Console.WriteLine("Bye");
+         }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using DELEGATES_CSHARP;
+using System;
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        //Delegate_demo_1 d = new Delegate_demo_1();
+        //myAction action;
+        //action = d.SayHello;
+        //action();
+        //action = d.SayBye;
+        //action();
+
+
+        //Delegate_demo_2 d1= new Delegate_demo_2();
+        //Action<String> notify=null;
+        //notify+=d1.SendMail;
+        //notify-= d1.SMS;
+        //notify+= d1.Log;
+
+        //notify("User Registered");
+
+
+        //Delegate_demo_2 d2=new Delegate_demo_2();
+        //Delegets_demo_3 d3=new Delegets_demo_3();
+        //callBack notify=null;
+        //notify = d2.SendMail;
+        //notify += d2.SMS;
+        //notify += d2.Log;
+        //d3.Register(notify);
+
+
+        Discount_service service= new Discount_service();
+
+        DiscountRule rule;
+
+        rule = service.FestivalDiscount;
+        rule += service.PremiumDiscount;
+        rule += service.CouponDiscount;
+
+        Console.WriteLine("Final price of the product:= "+ rule(1000));
+
+
+
+
+    }
+  
+
+}
