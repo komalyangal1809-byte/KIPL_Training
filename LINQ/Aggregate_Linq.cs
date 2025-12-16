523f0f7f03f738cb865bf8e2d99bde31092668d6
komalyangal1809-byte <komalyangal1809@gmail.com>
2025-12-16 12:08:02 +0530

    Updated files

---
+++ LINQ/Aggregate_Linq.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_CSharp
+{
+    internal class Aggregate_Linq
+    {
+        List<int> list=new List<int>() { 1,2,3,4};
+        List<string> names = new() { "Komal", "Ankita", "Sakshi" };
+        public void input()
+        {
+            var res = list.Aggregate(1,(acc,n)=>acc*n);
+            Console.WriteLine("Product of numbers is "+res);
+
+            var res1 = list.Aggregate(10,(acc,n)=>acc+10);
+            Console.WriteLine("Adding 10 to each number "+res1);
+
+            var res2 = names.Aggregate((a,b)=>a+"|"+b);
+            Console.WriteLine("Names are: "+res2);
+
+            List<int> empty=new List<int>() { };
+            Console.WriteLine("empty" + empty.Aggregate( 1,(acc, n) => acc * n));
+        }
+
+    }
+}
--- LINQ/Product_data_Joins.cs
+++ LINQ/Product_data_Joins.cs
@@ -12,35 +12,15 @@ namespace LINQ_CSharp
         public String ProductId { get; set; }
         public int CategoryId { get; set; }
 
-        public void input()
-        {
-            List<Product_data_Joins> list = new List<Product_data_Joins>()
-            {
-                 new Product_data_Joins { ProductId = "p01", ProductName = "Laptop", CategoryId = 1 },
-                 new Product_data_Joins { ProductId = "p02", ProductName = "Phone", CategoryId = 2 },
-                 new Product_data_Joins { ProductId = "p03", ProductName = "Headphone", CategoryId = 3 },
-                 new Product_data_Joins { ProductId = "p04", ProductName = "shirt", CategoryId = 4},
-            };
-           
 
-        }
-       
 
-             
     }
-    class Categories
+    class Category
     {
-        public String CategoryName { get; set; }
-        public int CategoryId { get; set; }
-
-        public void input()
-        {
-            List<Categories> list = new List<Categories>() {
-                new Categories {CategoryId=1,CategoryName="Electronic"},
-                new Categories {CategoryId=2,CategoryName="Electronic"},
-                new Categories{CategoryId=3,CategoryName="Electronic"},
-                new Categories{CategoryId=4,CategoryName="Clothing" }
-            };
-        }
+        public int CategoryId { get; set; } 
+        public string CategoryName { get; set; }
     }
 }
+   
+      
+       
