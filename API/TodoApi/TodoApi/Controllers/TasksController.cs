f3090e8a22d914d76982b173d1cc178ce34bc838
komalyangal1809-byte <komalyangal1809@gmail.com>
2025-12-17 18:50:57 +0530

    Added files

---
+++ API/TodoApi/TodoApi/Controllers/TasksController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        static List<string> tasks = new();
+
+        [HttpGet]
+        public List<string> GetTasks()
+        {
+            return tasks;
+        }
+        [HttpPost]
+
+        public void AddTask([FromBody] string task)
+        {
+            tasks.Add(task);
+        }
+        [HttpDelete]
+        public void DeleteTask([FromBody] string task)
+        {
+            tasks.Remove(task);
+        }
+    }
+}
